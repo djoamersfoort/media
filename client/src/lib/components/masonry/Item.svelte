3c57e07f090bb485efdf1fd5633d5b51cb67c98c
sverben <59171289+sverben@users.noreply.github.com>
2024-01-22 18:05:01 +0100

    Feat: add lazy loading

--- client/src/lib/components/masonry/Item.svelte
+++ client/src/lib/components/masonry/Item.svelte
@@ -35,7 +35,7 @@
             <Fa icon={faCheck} />
         {/if}
     </div>
-    <img src="{item.cover_path}" alt="" on:click={interact}>
+    <img src="{item.cover_path}" alt="" loading="lazy" on:click={interact}>
 </div>
 
 <style>
